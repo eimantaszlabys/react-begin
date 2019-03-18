83c512a809cdba81fe9b15b4b06bce76f51e1db9
Eimantas Žlabys <eimantas@metalface.com>
2019-03-18 16:33:21 +0200

    Configured webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,5 +12,31 @@ module.exports = {
     path: path.resolve(__dirname, "build"),
     pulicPath: "/",
     filename: "bundle.js"
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exlude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   }
 };
