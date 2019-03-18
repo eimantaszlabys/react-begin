cf1e3607bdb4d7c5c2726f1652a6e7f1eee84558
Eimantas Žlabys <eimantas@metalface.com>
2019-03-18 17:37:41 +0200

    eslint

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -31,7 +31,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ["babel-loader", "eslint-loader"]
       },
       {
         test: /(\.css)$/,
