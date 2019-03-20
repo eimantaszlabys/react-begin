541c4c5d3edbb758236f9a916e437ee13cd65403
Eimantas Žlabys <eimantas@metalface.com>
2019-03-20 12:51:52 +0200

    Geting data from API

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,23 @@
 import * as types from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course };
+}
+
+function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCESS, courses };
+}
+
+export function loadCourses() {
+  return function(dispach) {
+    return courseApi
+      .getCourses()
+      .then(courses => {
+        dispach(loadCourseSuccess(courses));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
 }
